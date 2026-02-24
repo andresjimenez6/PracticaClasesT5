05defeb9ef965173638a2eaa1d682c6aadd8a3ec
andresjimenez6 <andrescortijo2006@gmail.com>
2026-02-24 20:03:17 +0100

    creacion de clases

---
+++ JuegosOlimpicos/src/juegosolimpicos/DeportistasIndividuales.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package juegosolimpicos;
+
+/**
+ *
+ * @author ANDRES JIMENEZ
+ */
+public class DeportistasIndividuales extends Participante{
+    private String especialidad;
+    
+    public DeportistasIndividuales(){
+        this.especialidad="";
+    }
+    
+}
