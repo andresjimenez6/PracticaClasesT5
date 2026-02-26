d596ce5b111c8c57565e6954e794955dc488b260
andresjimenez6 <andrescortijo2006@gmail.com>
2026-02-26 08:29:35 +0100

    jdsaojdisa

--- JuegosOlimpicos/src/juegosolimpicos/DeportistasIndividuales.java
+++ JuegosOlimpicos/src/juegosolimpicos/DeportistasIndividuales.java
@@ -12,7 +12,22 @@ public class DeportistasIndividuales extends Participante{
     private String especialidad;
     
     public DeportistasIndividuales(){
+        super();
         this.especialidad="";
     }
+    
+    public DeportistasIndividuales(String especialidad, String nombre, String pais, int edad, int numeroDeIdentificacionOlimpico, int numeroTotalDeMedallasObtenidas){
+        super(nombre, pais, edad, numeroDeIdentificacionOlimpico, numeroTotalDeMedallasObtenidas);
+        this.especialidad = especialidad;
+        
+    }
+    
+    public DeportistasIndividuales(DeportistasIndividuales c){
+        super(c);
+        this.especialidad= c.especialidad;
+    }
+    
+    
+    
     
 }
